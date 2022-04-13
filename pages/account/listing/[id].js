efb1254dc3a2b035fa0a7672e303df340a45f591
AndrewKaranja <andywanji56@gmail.com>
2022-04-13 22:12:05 +0300

    Added ad calendar

--- pages/account/listing/[id].js
+++ pages/account/listing/[id].js
@@ -4,6 +4,10 @@ import { HeartIcon} from '@heroicons/react/outline'
 import { StarIcon } from '@heroicons/react/solid';
 import * as BiIcons from 'react-icons/bi';
 import * as RiIcons from 'react-icons/ri';
+import * as MdIcons from 'react-icons/md';
+import * as FcIcons from 'react-icons/fc';
+import { DateRange } from 'react-date-range';
+
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectFlip, Pagination, Navigation } from "swiper";
@@ -11,6 +15,8 @@ import "swiper/css";
 import "swiper/css/effect-flip";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 import Image from 'next/image';
 import billboard from '../../../images/cat.png';
@@ -39,6 +45,20 @@ export default function ListingDetails() {
   const{query:{listingid},}=useRouter();
   const [response, setResponse] = useState("");
 
+  const[startDate,setStartDate]=useState(new Date());
+  const[endDate,setEndDate]=useState(new Date());
+
+  const selectionRange={
+    startDate:startDate,
+    endDate:endDate,
+    key:'selection',
+}
+
+const handleSelect=(ranges)=>{
+    setStartDate(ranges.selection.startDate)
+    setEndDate(ranges.selection.endDate)
+}
+
   useEffect(() => {
     async function getListingDetail(user){
       const docRef = doc(db, `users/${user.id}/listings`, "f7tU7trC0zWSriZav6Uo");
@@ -62,7 +82,7 @@ export default function ListingDetails() {
 
  
     
-    <div className='flex m-12   h-84 hover:border-2 hover:shadow-sm rounded-xl bg-white cursor-pointer select-none transition duration-100 ease-out '>
+    <div className='flex mt-12 ml-12 mr-12 mb-2   h-84  rounded-xl bg-white cursor-pointer select-none '>
     
       <div className='flex flex-[10]'>
       <div className='relative h-full w-32 flex-grow-[1] '>
@@ -72,27 +92,36 @@ export default function ListingDetails() {
             <div className='flex flex-col flex-grow-[9] p-5 h-fit'>
                 <div className='flex justify-between'>
                 <h4 className='text-xl text-black font-bold'>Gigantic Billboard</h4>
-                    <button className='hidden md:inline h-8 border-2 border-red-600 bg-red-200 rounded text-black hover:bg-orange-300 p-2'
-              >Unlisted</button>
+                    
+              <RiIcons.RiMenu4Fill/>
                     
                 </div>
                 
-                <p className='text-sm text-gray-500'>Near Strathmore</p>
+                <p className='text-sm text-gray-500 font-semibold'>📍Near Strathmore</p>
                 
-                <p className='pt-2 text-sm text-gray-500 flex-grow'>DESIGN . MOUNTING . POTATOES</p>
-    
-                <div className='flex justify-between items-end pt-5'>
-                <p className='flex items-center'>
-                <StarIcon className='h-5 text-[#FAB038]'/>
-               2.0
-                </p>
-                <div>
-                    <p className='text-lg lg:text-2xl font-semibold pb-2'>404KES/page</p>
+                <div className='flex flex-row m-2 items-center font-semibold text-gray-500'>
+               
+                <div className='flex flex-row m-2 items-center'>
+                  <MdIcons.MdOutlinePeopleAlt />
+                  <p className='px-1 '>6 clients</p>
                 </div>
-            </div>
+                <div className='flex flex-row m-2 items-center'>
+                  <MdIcons.MdOutlineReport />
+                  <p className='px-1 '>10 requests</p>
+                </div>
+
+                </div>
+            
             <button className=' border-2 border-orange-200 bg-[#FAB038]  text-white font-semibold hover:bg-orange-300 p-2 w-56 rounded-full'
-              >📅Check Ad Calendar</button>
+              >📅 Check Ad Calendar</button>
+              
             </div>
+            <DateRange
+              ranges={[selectionRange]}
+              minDate={new Date()}
+              rangeColors={["#FAB038"]}
+              
+              onChange={handleSelect}/>
 
       </div>
         
@@ -101,21 +130,36 @@ export default function ListingDetails() {
         </div>
 
         {/* main body */}
-        <div className='flex m-12   h-84 hover:border-2 hover:shadow-sm rounded-xl bg-white cursor-pointer select-none transition duration-100 ease-out '>
+        <div className='flex mt-3 ml-12 mr-12 mb-12 h-84  rounded-xl bg-white cursor-pointer select-none '>
     
     <div className='flex flex-[10]'>
     <div className='h-full  w-[70%] flex-grow-[6] m-5 '>
-          
-    <h4 className='text-xl text-black font-bold m-2'>Gigantic Billboard</h4>
+
+       <div>
+       <h4 className='text-xl text-black font-bold m-2'>Ad Description</h4>
+       <p className='m-5'>I just wanna tell you how I am feeling
+Gotta make you understand
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you</p>
+         </div>   
+    
 
 
-  <div className='border-b pt-2 border-black'/>
+  <div className='border-b pt-2 border-orange-100'/>
 <div className='my-5 flex flex-row'>
-<div className='flex flex-row items-center'>
+<div className='flex flex-row items-center mx-3'>
 <BiIcons.BiCrop className='h-8 w-8'/>
+<div className='ml-2'>
 <p className='text-sm text-black'>12 x 12 m</p>
+<p className='text-sm text-gray-500'>dimensions</p>
 </div>
-<div className='flex flex-row items-center'>
+
+</div>
+<div className='flex flex-row items-center mx-3'>
 
 <RiIcons.RiLightbulbFlashLine className='h-8 w-8'/>
 <p className='text-sm text-black'>Visible at Night</p>
@@ -123,17 +167,17 @@ export default function ListingDetails() {
 
 </div>
 
-<div className='border-b pt-2 border-black'/>
+<div className='border-b pt-2 border-orange-100'/>
 <div className='my-3'>
-<h4 className='text-sm font-semibold mb-3'>Services</h4>
-<p className='text-sm text-black bg-orange-100 w-fit p-2 rounded-sm font-medium'>Design</p>
+<h4 className='font-bold text-xl m-5'>Services</h4>
+<p className='text-sm text-black bg-orange-50 w-fit m-4 p-2 rounded-xl font-medium'>Design</p>
 </div>
 
 
-<div className='border-b pt-2 border-black'/>
+<div className='border-b pt-2 border-orange-100'/>
 
 <div>
-<h4>Property overview</h4>
+<h4 className='font-bold text-xl m-4'>Ad Space overview</h4>
 {/* <div className='flex  p-3'>
 <Image src={billboard} alt='ad image' width={100} height={100}  objectFit="fit" className='rounded-2xl m-5'/>
 <Image src={billboard} alt='ad image' width={100} height={100} objectFit="cover" className='rounded-2xl m-5'/>
@@ -148,7 +192,7 @@ export default function ListingDetails() {
         pagination={true}
         navigation={true}
         modules={[EffectFlip, Pagination, Navigation]}
-        className="mySwiper w-80 h-80"
+        className="mySwiper w-[60vw] h-80"
       >
         <SwiperSlide>
         <Image src={billboard} alt='ad image' layout='fill'  objectFit="cover" className='rounded-2xl m-5'/>
@@ -166,6 +210,35 @@ export default function ListingDetails() {
         <Image src={billboard} alt='ad image' layout='fill'  objectFit="cover" className='rounded-2xl m-5'/>
         </SwiperSlide>
       </Swiper>
+      {/* <Swiper
+        slidesPerView={2}
+        spaceBetween={30}
+        slidesPerGroup={2}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper w-[60vw] h-80"
+      >
+      <SwiperSlide>
+        <Image src={billboard} alt='ad image' layout='fill'  objectFit="cover" className='rounded-2xl m-5'/>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Image src={billboard} alt='ad image' layout='fill'  objectFit="cover" className='rounded-2xl m-5'/>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Image src={billboard} alt='ad image' layout='fill'  objectFit="cover" className='rounded-2xl m-5'/>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Image src={billboard} alt='ad image' layout='fill'  objectFit="cover" className='rounded-2xl m-5'/>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Image src={billboard} alt='ad image' layout='fill'  objectFit="cover" className='rounded-2xl m-5'/>
+        </SwiperSlide>
+      </Swiper> */}
 
 </div>
 
@@ -175,10 +248,16 @@ export default function ListingDetails() {
               
 
           </div>
-          <div className='flex flex-col flex-grow-[4] p-5 h-fit text-white bg-black rounded-xl m-5'>
-              <div className='flex '>
+          <div className='flex flex-col flex-grow-[4] h-fit'>
+            <div className='p-5 h-fit text-white bg-black rounded-xl m-5'>
+            <div className='flex '>
               <Image src={billboard} alt='ad image' width={40} height={40} objectFit="cover" className='rounded-2xl'/>
-              <p className='text-sm ml-2 font-bold  justify-center'>Mary Jane</p>
+              <div className='flex ml-2  flex-col'>
+                <p className='text-sm font-bold  justify-center'>Mary Jane</p>
+              <p className='text-xs '>Current client</p>
+
+              </div>
+              
               
              
               </div>
@@ -203,8 +282,21 @@ export default function ListingDetails() {
               
 
   
-          <button className=' border-2 border-orange-200 bg-blue-500  text-white font-semibold hover:bg-orange-300 p-2  rounded-xl'
+          <button className=' border-2 border-orange-200 bg-blue-500 w-full  text-white font-semibold hover:bg-orange-300 p-2  rounded-xl'
             >📫Send Messages</button>
+
+            </div>
+            {/* Client transactions documents */}
+            <div className='mx-4'>
+              <div className='flex flex-row items-center w-full justify-evenly '>
+                <FcIcons.FcDocument className='w-8 h-8 pr-1'/>
+                <p className='font-semibold text-sm justify-start'>Contract Document</p>
+                <RiIcons.RiDownload2Fill className='text-gray-400 content-end'/>
+                
+              </div>
+              <div className='border-b mx-4 m-2 border-orange-100'/>
+            </div>
+              
           </div>
 
     </div>
