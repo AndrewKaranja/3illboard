1ff2a772b1c01171ca0ca267a5e9456daa466f2d
AndrewKaranja <andywanji56@gmail.com>
2022-05-25 16:30:52 +0300

    Improved navigation(settings)

--- components/dashboard/Sidebar.js
+++ components/dashboard/Sidebar.js
@@ -269,7 +269,7 @@ function Sidebar({ sidebarOpen,
                         </li>
                         
                         <li className="mb-1 last:mb-0">
-                          <Link passHref href="/account/settings/plans" className="block  text-slate-400 hover:text-slate-200 transition duration-150 truncate">
+                          <Link passHref href="/account/settings/pricing" className="block  text-slate-400 hover:text-slate-200 transition duration-150 truncate">
                             <span className="text-sm hover:cursor-pointer text-white font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Plans</span>
                           </Link>
                         </li>
