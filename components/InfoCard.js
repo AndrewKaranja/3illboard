0e43238d2aaf0b1f1a9a138d229554a692b71ea3
AndrewKaranja <andywanji56@gmail.com>
2022-06-12 22:34:48 +0300

    Improved search page to be mobile friendly

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -2,15 +2,17 @@ import { HeartIcon} from '@heroicons/react/outline'
 import { StarIcon } from '@heroicons/react/solid'
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useMediaQuery } from 'react-responsive';
 
 function InfoCard({img,id,location,title,description,star,price,total}) {
     const router=useRouter();
+    const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
     const viewListing=()=>{
         router.push(`/search/${id}`);
       }
   return (
-    <div  onClick={viewListing} className='flex py-7 px-2 border-b cursor-pointer select-none hover:opacity-80 hover:shadow-lg  transition duration-100 ease-out first:border-t'>
-        <div className='relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0'>
+    <div  onClick={viewListing} className='flex flex-col sm:flex-row py-7 px-2 border-b cursor-pointer select-none hover:opacity-80 hover:shadow-lg  transition duration-100 ease-out first:border-t'>
+        <div className='relative h-36 w-full md:h-52 md:w-80 flex-shrink-0'>
         <Image src={img} layout="fill" alt='ad image' objectFit='cover' className='rounded-2xl'/>
         </div>
         <div className='flex flex-col flex-grow pl-5'>
@@ -21,7 +23,8 @@ function InfoCard({img,id,location,title,description,star,price,total}) {
             </div>
             <h4 className='text-xl'>{title}</h4>
             <div className='border-b w10 pt-2'/>
-            <p className='pt-2 text-sm text-gray-500 flex-grow'>{description}</p>
+            {!isMobile && <p className='pt-2 text-sm text-gray-500 flex-grow'>{description}</p>}
+            
 
             <div className='flex justify-between items-end pt-5'>
             <p className='flex items-center'>
@@ -30,7 +33,7 @@ function InfoCard({img,id,location,title,description,star,price,total}) {
             3.5
             </p>
             <div>
-                <p className='text-lg lg:text-2xl font-semibold pb-2'>KES{price.price}/{price.interval}</p>
+                <p className='text-xs font-semibold pb-2'>KES{price.price}/{price.interval}</p>
                 {/* <p className='text-right font-extralight'>{total}</p> */}
             </div>
         </div>
--- pages/search/index.js
+++ pages/search/index.js
@@ -1,7 +1,8 @@
 import { format } from 'date-fns';
 import {useRouter} from "next/router";
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import Head from 'next/head';
+import React, { useState,useEffect} from 'react';
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import InfoCard from '../../components/InfoCard';
@@ -17,12 +18,21 @@ import imgside from '../../images/street-sideAd.png';
 import imgclock from '../../images/clock.png';
 import imgmoving from '../../images/movingAd.png';
 import * as AiIcons from 'react-icons/ai';
+import { useMediaQuery } from 'react-responsive';
 
 function Search({listings,searchResults}) {
   const router=useRouter();
   const {user}=useAuth();
 const {location,startDate,endDate}=router.query;
 const range="today";
+const [showMap, setShowMap] = useState(true);
+const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+
+useEffect(() => {
+  if(!isMobile){
+    setShowMap(true);
+  }
+}, [isMobile])
 
 
 
@@ -35,38 +45,49 @@ if(startDate && endDate){
 }
 
   return (
-    <div>
+    <div className="flex flex-col">
+      <Head>
+        <title>Search</title>
+        <meta name="description" content="A billboard marketplace" />
+        <link rel="icon" href="/3illboardLogoMini.ico" />
+      </Head>
         <Header placeholder={location ?`${location} | ${range} `:"Search Location"}/>
-        <div className="flex justify-between bg-white">
-          <div className="flex">
-          <div className="m-2 text-center border-b-2 border-b-[#fab038] hover:border-b-2 hover:border-b-black hover:cursor-pointer">
+        <div className="flex py-1 justify-between w-full bg-white z-40">
+          <div className="flex overflow-auto max-w-full">
+            <div className="mx-2 text-center border-b-2 border-b-[#fab038] hover:border-b-2 hover:border-b-black hover:cursor-pointer">
             <Image src={imgall} width={40} height={40} className='mx-auto' alt='pic'/>
-            <p className="text-xs font-bold text-gray-600">All</p>
+            <p className="text-xs font-bold text-gray-600 w-12"> All </p>
             </div>
-            <div className="m-2 text-center hover:border-b-2 hover:border-b-black hover:cursor-pointer">
+            <div className="mx-2 text-center hover:border-b-2 hover:border-b-black hover:cursor-pointer">
             <Image src={imgbill} width={40} height={40} className='mx-auto' alt='pic'/>
             <p className="text-xs font-bold text-gray-600">Print Billboard</p>
             </div>
-            <div className="m-2 text-center hover:border-b-2 hover:border-b-black hover:cursor-pointer">
+            <div className="mx-2 text-center hover:border-b-2 hover:border-b-black hover:cursor-pointer">
             <Image src={imgdigi} width={40} height={40} className='mx-auto' alt='pic'/>
             <p className="text-xs font-bold text-gray-600">Digital Billboard</p>
             </div>
-            <div className="m-2 text-center hover:border-b-2 hover:border-b-black hover:cursor-pointer">
+            <div className="mx-2 text-center hover:border-b-2 hover:border-b-black hover:cursor-pointer">
             <Image src={imgclock} width={40} height={40} className='mx-auto' alt='pic'/>
             <p className="text-xs font-bold text-gray-600">Clock Ads</p>
             </div>
-            <div className="m-2 text-center hover:border-b-2 hover:border-b-black hover:cursor-pointer">
+            <div className="mx-2 text-center hover:border-b-2 hover:border-b-black hover:cursor-pointer">
             <Image src={imgside} width={40} height={40} className='mx-auto' alt='pic'/>
             <p className="text-xs font-bold text-gray-600">Lampost Ads</p>
             </div>
 
           </div>
-            <div><button className="flex button mt-3 m-1"><AiIcons.AiOutlineFilter className="pt-1" /><p>Filter</p></button></div>
+          {!isMobile && <div><button className="flex button mt-3 m-1"><AiIcons.AiOutlineFilter className="pt-1" /><p>Filter</p></button></div>}
             
+            {/* Remember to fix the ismobile showmap bug */}
           </div>
-        <main className='flex w-1/2'>
-         
-            <section className='flex-grow pt-6 px-6 '>
+        <main className="flex-col lg:flex">
+          {showMap && <div className="  lg:right-0 h-[70vh] lg:h-full z-0 lg:fixed lg:top-0 w-full lg:w-[40vw] "><Map searchResults={listings}/></div> }
+        
+        {isMobile && <div className=" bg-black w-full h-[10vh] text-center">
+          <p className="font-extrabold rounded-t-xl bg-white text-3xl px-auto h-full">_</p>
+        </div> }
+        
+         <section className='pt-6 px-6 lg:w-[60vw] w-full '>
                 <p className='text-xs mb-2'>200+ Ad spaces available {range} in { location ?`${location}`:"the area"}</p>
     
                 <div className='flex flex-col'>
@@ -88,12 +109,25 @@ if(startDate && endDate){
 
             </section>
 
+            
+
+
            
-            <div className="fixed top-0 right-0 z-0"><Map searchResults={listings}/></div>
+            
+
+        
+            
             
         </main>
+        {isMobile && <button className='sticky bottom-6 mx-auto px-4 border-2 bg-black rounded-full border-[#FAB038] text-white hover:bg-orange-300 p-2'
+          onClick={()=>{setShowMap(!showMap)}}>{showMap ?'Show List':'Show Map'}</button>}
+        
        
-          <Footer/>
+       <div  className="sticky z-50">
+       <Footer/>
+
+       </div>
+          
 
        
         
