b819e6c4e8b358494b82700e0ae0f6d5dd7d70c6
AndrewKaranja <andywanji56@gmail.com>
2022-06-23 13:23:11 +0300

    configured fullcalendar to work with next js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/react",
+  "@fullcalendar/interaction",
+  
+]);
+module.exports = withTM({
   reactStrictMode: true,
   images:{
-    domains:['links.papareact.com']
-  },
-}
+    domains:['links.papareact.com','firebasestorage.googleapis.com']
+  }
+});
