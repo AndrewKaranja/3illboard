87dacf66f80e4103bbea1a3aa31d9814e72041b9
AndrewKaranja <andywanji56@gmail.com>
2022-06-23 13:26:58 +0300

    Improved new request table

--- components/dashboard/NewRequestsCard.js
+++ components/dashboard/NewRequestsCard.js
@@ -5,14 +5,16 @@ import {useRouter} from "next/router";
 import { useAuth } from '../../context/AuthContext';
 import { collection, query, where, getDocs,orderBy,limit } from "firebase/firestore";
 import { db } from '../../firebase';
-import UserIcon from "../../images/ferret.png"
+import UserIcon from "../../images/ferret.png";
+import { Badge,Table } from '@mantine/core';
 
 function NewRequestsCard() {
   const {user}=useAuth();
     const router = useRouter();
     const [requests,setRequests] =useState([]);
     const [done, setDone] = useState(undefined);
-
+   
+ 
 
     const handleRequestedListing=(listingid)=>{
       if(requests){
@@ -48,26 +50,48 @@ function NewRequestsCard() {
     getRequests(user);
    
   
-  }, [user])
+  }, [user]);
+  const requestRows = requests && requests?.map((request)=>(
+      
+    <tr key={request.id} >
+      <td>
+      <Image src={UserIcon}alt='user image' objectFit="contain" width={50} height={50} /></td>
+      
+      <td>{request?.fname}</td>
+      <td>{request.requestedPeriod ? request?.requestedPeriod: "Not set"}</td>
+      <td><Badge onClick={()=>{handleRequestedListing(request?.listingid)}} color="green" variant="light">
+        Listing
+    </Badge></td>
+      <td><button onClick={()=>{handleRespondClick(request?.chatid)}} className='bg-[#fab038] text-white text-xs p-1 h-fit font-semibold rounded'>Respond</button></td>
+    </tr>
+    
+  ));
+
   return (
     <div className='bg-white shadow-lg rounded-sm border border-slate-200'>
         <header className="px-5 py-4 border-b border-slate-100">
         <h2 className="font-semibold text-slate-800">New Requests</h2>
       </header>
-      {requests && requests?.map((request)=>(
-      <div key={request?.requestTitle} className='flex flex-row p-2'>
-      {/* <Avatar size="40" round={true}  /> */}
-      <Image src={UserIcon}alt='user image' objectFit="contain" width={50} height={50} />
-          <div className='text-ellipsis overflow-hidden w-32 mx-3  whitespace-nowrap inline-block'>
-              <h4 className='font-bold text-sm'>{request?.fname}</h4>
-              <p className='text-xs'>{request?.email}</p>
-          </div>
-          <div onClick={()=>{handleRequestedListing(request?.listingid)}}  className='text-xs text-ellipsis overflow-hidden hover:text-[#fab038] hover:cursor-pointer  whitespace-nowrap inline-block'>A beautiful billboard in ngong</div>
-          <div><h4 className='font-semibold text-sm text-gray-700'>{request?.requestedPeriod}</h4></div>
-          <button onClick={()=>{handleRespondClick(request?.chatid)}} className='bg-[#fab038] text-white text-xs p-1 h-fit font-semibold rounded'>Respond</button>
+      
+      <div  className='flex flex-row p-2'>
+        <Table highlightOnHover fontSize="xs" >
+      <thead>
+        <tr>
+          <th>Avatar</th>
+          <th>Name</th>
+          
+          <th>Requested Period</th>
+          <th>View</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody className='hover:cursor-pointer'>{requestRows}</tbody>
+    </Table>
+
+
 
       </div>
-   ))}
+
       
     </div>
   )
