cbfd958026428ece2a4b1a9d0abf906aaf002332
Andrew Karanja <andywanji56@gmail.com>
2022-07-18 15:03:30 +0300

    Updated Sidebar UI-replaced messages with inbox

--- components/dashboard/Sidebar.js
+++ components/dashboard/Sidebar.js
@@ -25,28 +25,7 @@ function Sidebar({ sidebarOpen,
 
  const trigger = useRef(null);
   const sidebar = useRef(null);
-  // const trigger = React.forwardRef(null);
-  // const sidebar=React.forwardRef(null);
 
-  // NOTE THIS IS A TEMPORARY FIX
-  //!!!!!!!!!!!
-  //!!!!!!!!!!
-  //!!!!!!!!!
-  //!!!!!!!!
-
-  //const storedSidebarExpanded=useRef(null);
-//   const ISSERVER = typeof window === "undefined";
-
-// if(!ISSERVER) {
-//  // Access localStorage
-//  const storedSidebarExpanded = localStorage.getItem('sidebar-expanded');
-//  return storedSidebarExpanded;
-// }else{
-//   storedSidebarExpanded=null;
-//   return storedSidebarExpanded;
-// }
-
-  //This is
   const [storedSidebarExpanded, setstoredSidebarExpanded] = useState('true');
 
   useEffect(() => {
@@ -176,8 +155,8 @@ function Sidebar({ sidebarOpen,
                 </div>
               </Link>
             </li>
-                {/* Messages */}
-                <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${router.pathname.includes('messages') && 'bg-slate-900'}`}>
+                  {/* Messages */}
+                {/* <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${router.pathname.includes('messages') && 'bg-slate-900'}`}>
               <Link passHref href="/account/messages" className={`block text-slate-200 hover:text-white truncate transition duration-150 ${router.pathname.includes('messages') && 'hover:text-slate-200'}`}>
                 <div className="flex items-center hover:cursor-pointer">
                   <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
@@ -187,8 +166,21 @@ function Sidebar({ sidebarOpen,
                   <span className="text-sm text-white font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Messages</span>
                 </div>
               </Link>
+            </li> */}
+
+            {/* Inbox */}
+            <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${router.pathname.includes('inbox') && 'bg-slate-900'}`}>
+              <Link passHref href="/account/inbox" className={`block text-slate-200 hover:text-white truncate transition duration-150 ${router.pathname.includes('inbox') && 'hover:text-slate-200'}`}>
+                <div className="flex items-center hover:cursor-pointer">
+                  <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
+                    <path className={`fill-current text-slate-600 ${router.pathname.includes('inbox') && 'text-indigo-500'}`} d="M14.5 7c4.695 0 8.5 3.184 8.5 7.111 0 1.597-.638 3.067-1.7 4.253V23l-4.108-2.148a10 10 0 01-2.692.37c-4.695 0-8.5-3.184-8.5-7.11C6 10.183 9.805 7 14.5 7z" />
+                    <path className={`fill-current text-slate-400 ${router.pathname.includes("inbox") && 'text-[#fab038]'}`} d="M11 1C5.477 1 1 4.582 1 9c0 1.797.75 3.45 2 4.785V19l4.833-2.416C8.829 16.85 9.892 17 11 17c5.523 0 10-3.582 10-8s-4.477-8-10-8z" />
+                  </svg>
+                  <span className="text-sm text-white font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Inbox</span>
+                </div>
+              </Link>
             </li>
-           
+           {router.pathname.endsWith("inbox")}
             
             {/* Analytics */}
             {/* <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${router.pathname.includes('analytics') && 'bg-slate-900'}`}>
