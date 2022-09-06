4cc7f90fea6927589d782f0b500723ed06435809
Andrew Karanja <38155560+AndrewKaranja@users.noreply.github.com>
2022-09-06 11:42:52 +0300

    Changed Banner Headline

--- components/Banner.js
+++ components/Banner.js
@@ -11,7 +11,7 @@ function Banner() {
              layout='fill'
              objectFit='cover'/>
              <div className='absolute top-1/2 w-full text-center'>
-                 <p className='text-sm sm:text-lg text-white'>Not sure where to advertise? Perfect. We bring the 3*ll😉 in advertisment</p>
+                 <p className='text-sm sm:text-lg text-white'>Not sure where to advertise? Perfect.</p>
                 <Link href='/search' passHref>
                  <button className='text-white bg-[#FAB038] px-5 py-2 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>Discover</button>
                 </Link>
