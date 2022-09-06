ab049cd1e7956068f73fe0c861d4fb72b84a117f
Andrew Karanja <38155560+AndrewKaranja@users.noreply.github.com>
2022-09-06 11:51:46 +0300

    Changed image size

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 function MediumCard({img,title}) {
   return (
     <Link href='/search' passHref><div className='cursor-pointer py-10 px-3 select-none hover:scale-105 transform transition duration-300 ease-out'>
-        <div className='relative h-72 w-72'>
+        <div className='relative h-44 w-44'>
             <Image src={img} layout="fill" alt='medium image' className='rounded-xl '/>
         </div>
         <h3 className='block text-2xl mt-3 mx-auto text-center font-semibold'>{title}</h3>  
